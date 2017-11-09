aa08712ab78a067c3e546d8c8dc66479bbf81c1d
iceycc <w@icey.cc>
2017-11-09 20:53:42 +0800

    4-index交互基本实现

--- public/backEnd/js/admin.js
+++ public/backEnd/js/admin.js
@@ -1,1 +1,38 @@
 // 该文件的功能是用来写首页的js交互的
+
+/*不显示转圈效果*/
+NProgress.configure({
+  showSpinner: false
+});
+
+// 在ajax请求的时候 把进度条显示出来
+$(window).ajaxStart(function () {
+  NProgress.start()
+});
+$(window).ajaxComplete(function () {
+  NProgress.done()
+});
+
+// 点击顶部按钮隐藏显示左侧栏
+
+$(".ypg-nav a:first-child").on("click",function () {
+  console.log(1)
+  $(".ypg-aside").toggle()
+
+  //TODO:想想怎么加上动画
+  $(".ypg-section").toggleClass("move")  
+})
+
+
+// 点击左侧侧边栏中的选项显示折叠选项
+
+$(".ypg-aside .menu").on("click","[href='javascript:;']",function () { 
+  var $_this = $(this)
+  console.log(1)
+  var $child = $_this.siblings()
+  var $parent = $_this.parent()
+  // $parent.toggleClass("active")
+
+
+  // $child.slideToggle()
+})
